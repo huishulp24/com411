69d1218fd2753640c3238e5bf3380c6d2c94be5f
repl.it user <replituser@example.com>
2021-03-06 20:29:51 +0000

    Added code to show robot eyes

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,33 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+
+# Ask user for eye character 
+print("Please enter an eye character")
+eye=input()
+# Display an ascii art robot
+print("##########")
+print("#  " + eye + "  " + eye +"  #")
+print("#        #")
+print("##########")
+
+# Ask user for eye character 
+print("""
+##########
+#        #
+#        #
+##########
+""")
+
+# Ask user for eye character 
+print("Please enter an eye character")
+eye=input()
+# Display an ascii art robot
+print("""
+##########
+#  """ + eye +"""  """ + eye + """  #
+#        #
+##########
+""")
