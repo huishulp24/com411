9360a1851a0f30174478bc458b0dca081bebe336
repl.it user <replituser@example.com>
2021-03-08 11:35:22 +0000

    Äddedcode

---
+++ basics/decision/simple_decision/if.py
@@ -0,0 +1,6 @@
+# Ask user for the type of books like
+print("What type of book is this?")
+book_type=input()
+if(book_type == "adventure"):
+ print("\nI like adventure books!")
+print("\nFinished reading book.")
