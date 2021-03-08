11d474eb7c7843f8ca851ab3131e47c78d9b0592
repl.it user <replituser@example.com>
2021-03-08 12:04:47 +0000

    Added code if_else

---
+++ basics/decision/simple_decision/if_else.py
@@ -0,0 +1,8 @@
+# Ask user for the type of activity
+print("Please enter the activity to be performed")
+activity = input()
+if(activity == "calculate"):
+   print("\nPerforming activity calculation!")
+else:
+  print("\nPerforming activity...!")
+print("\nActivity completed!")
