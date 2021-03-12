94734d70b97736ad6e7070ffd43604b08145887c
repl.it user <replituser@example.com>
2021-03-12 13:19:33 +0000

    And operator

---
+++ basics/decision/and_operator.py
@@ -0,0 +1,9 @@
+print("What did I hear?")
+sound = input()
+print("What did I see?")
+see = input()
+if((sound == "hoor") and (see == "red eyes")):
+  print("I am a little scared but I will continue!")
+else:
+  if((sound == "grrr") and (see == "many red eyes")):
+    print("There is a scary creature. I should get out of here")
